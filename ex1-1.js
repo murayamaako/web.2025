e7f0ae0cfc06a23ab0b63043677bea0153a6bc78
murayamaako <167832805+murayamaako@users.noreply.github.com>
2025-01-09 15:35:06 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,2 +1,2 @@
-function imgchange5(){document.getElementById('moana').src='./image/1.jpg';}
-function mes2(){document.getElementById('moana').src='./image/0.jpg';}
+function imgchange1(){document.getElementById('bts').src='./image/bts1.jpg';}
+function mes2(){document.getElementById('bts').src='./image/bts2.jpg';}
